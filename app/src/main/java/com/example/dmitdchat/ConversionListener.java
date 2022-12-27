0ddd38084f60ebf7a8d1a5670442ac92879468c4
DmitD8 <diagilev.d@gmail.com>
2022-12-27 20:58:52 +0300

    DmitDchat

---
+++ app/src/main/java/com/example/dmitdchat/ConversionListener.java
@@ -0,0 +1,5 @@
+package com.example.dmitdchat;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/dmitdchat/UserListener.java
@@ -0,0 +1,5 @@
+package com.example.dmitdchat;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
